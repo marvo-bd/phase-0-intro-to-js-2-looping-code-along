d36275096eb9ecaa68881e78363d324ae37ada27
Marvin Kabira <marvobd@gmail.com>
2024-04-28 20:43:18 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
